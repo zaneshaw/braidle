11dcdff5ac991695433560dacd9c04508e92c0b8
Zane Shaw <zane@squidee.dev>
2026-01-06 02:54:09 +1100

    add pipe image to game button
    probably temporary

--- src/lib/components/GameButton.svelte
+++ src/lib/components/GameButton.svelte
@@ -2,10 +2,10 @@
 	let { href, iconSrc, label, description }: { href: string; iconSrc: string; label: string; description: string } = $props();
 </script>
 
-<a {href} class="group interaction-arrow flex cursor-pointer gap-4 bg-red-500/20 p-2">
+<a {href} class="group interaction-arrow flex cursor-pointer gap-4 bg-[url('/images/pipe.png')] bg-contain bg-center bg-no-repeat px-5 py-2">
 	<img src={iconSrc} alt="Game icon" class="h-16" />
 	<div class="flex grow flex-col justify-center text-left">
-		<h2 class="group-hover:animate-[flash-red_0.6s_infinite_linear_alternate]">{label}</h2>
+		<h2 class="mt-1 leading-6 group-hover:animate-[flash-red_0.6s_infinite_linear_alternate]">{label}</h2>
 		<span>{description}</span>
 	</div>
 </a>

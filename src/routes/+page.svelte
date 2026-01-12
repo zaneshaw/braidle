047fa51e2684b99b7aae942034838cd679838738
Zane Shaw <zane@squidee.dev>
2026-01-13 00:49:13 +1100

    reorder game buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <div class="flex flex-col gap-3">
-	<GameButton href="/image" iconSrc="/images/temp_door.png" label="Image" description="Guess the level from a cropped image." disabled />
+	<GameButton href="/braidoku" iconSrc="/images/temp_door.png" label="Braidoku" description="Guess the levels from their categories." />
 	<GameButton href="/puzzle_piece" iconSrc="/images/temp_door.png" label="Puzzle Piece" description="Guess the level from a puzzle piece." />
-	<GameButton href="/braidoku" iconSrc="/images/temp_door.png" label="Braidoku" description="Guess the levels from their categories." disabled />
+	<GameButton href="/image" iconSrc="/images/temp_door.png" label="Image" description="Guess the level from a cropped image." disabled />
 	<GameButton href="/elements" iconSrc="/images/temp_door.png" label="Elements" description="Guess the level from its count of elements." disabled />
 </div>

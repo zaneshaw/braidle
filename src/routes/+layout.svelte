22c28903d6766701ddd8670152d80d65d8f1756b
Zane Shaw <zane@squidee.dev>
2026-01-26 00:59:35 +1100

    reinit project with sv create

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,39 +1,9 @@
 <script lang="ts">
-	import { afterNavigate } from "$app/navigation";
-	import "./layout.css";
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
-
-	let interactionArrow: HTMLElement;
-
-	function handleMouseMove(e: MouseEvent) {
-		const target = e.target as HTMLElement;
-
-		interactionArrow.style.top = `${e.clientY - interactionArrow.clientHeight - 15}px`;
-		interactionArrow.style.left = `${e.clientX - interactionArrow.clientWidth / 2}px`;
-		interactionArrow.style.opacity = target.closest(".interaction-arrow") ? "1" : "0";
-	}
-
-	afterNavigate(() => {
-		interactionArrow.style.opacity = "0";
-	});
 </script>
 
-<svelte:document onmousemove={handleMouseMove} />
-
-<svelte:head>
-	<title>Braidle</title>
-	<link rel="icon" href="/favicon.ico" />
-</svelte:head>
-
-<div class="mx-auto flex h-full w-100 flex-col gap-5 py-5">
-	<div class="text-center">
-		<a href="/" class="group interaction-arrow cursor-pointer">
-			<h1 class="transition-transform group-hover:scale-105">Braidle</h1>
-		</a>
-		<h3>Test your Braid knowledge</h3>
-	</div>
-	{@render children()}
-</div>
-
-<img bind:this={interactionArrow} src="/images/interaction_arrow.png" alt="" class="pointer-events-none fixed z-100 w-12 opacity-0 transition-opacity select-none" />
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,2 @@
-<script>
-	import GameButton from "$lib/components/GameButton.svelte";
-</script>
-
-<div class="flex flex-col gap-3">
-	<GameButton href="/braidoku" iconSrc="/images/temp_door.png" label="Braidoku" description="Guess the levels from their categories." />
-	<GameButton href="/puzzle_piece" iconSrc="/images/temp_door.png" label="Puzzle Piece" description="Guess the level from a puzzle piece." />
-	<GameButton href="/image" iconSrc="/images/temp_door.png" label="Image" description="Guess the level from a cropped image." disabled />
-	<GameButton href="/elements" iconSrc="/images/temp_door.png" label="Elements" description="Guess the level from its count of elements." disabled />
-</div>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
